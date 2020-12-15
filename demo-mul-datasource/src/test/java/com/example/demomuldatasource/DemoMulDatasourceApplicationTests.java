5d7f60280942eda07fd54daf10bdc5d3904a5b88
yangshenrui <yangshenrui@gmail.com>
2020-12-15 15:06:41 +0800

    多数据源配置

---
+++ demo-mul-datasource/src/test/java/com/example/demomuldatasource/DemoMulDatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demomuldatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMulDatasourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
