8fdcf3cb698c140203308801f96cb009e4ac8328
yangshenrui <yangshenrui@gmail.com>
2020-12-16 09:00:07 +0800

    MySQL单数据源

--- demo-mul-datasource/src/main/java/com/example/demomuldatasource/DemoMulDatasourceApplication.java
+++ demo-mul-datasource/src/main/java/com/example/demomuldatasource/DemoMulDatasourceApplication.java
@@ -1,13 +1,35 @@
 package com.example.demomuldatasource;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @SpringBootApplication
-public class DemoMulDatasourceApplication {
+@Slf4j
+public class DemoMulDatasourceApplication implements CommandLineRunner {
+	@Autowired
+	private DataSource dataSource;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoMulDatasourceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws SQLException {
+		showConnection();
+	}
+
+	private void showConnection() throws SQLException {
+		log.info(dataSource.toString());
+		Connection conn= dataSource.getConnection();
+		log.info(conn.toString());
+		conn.close();
 	}
 
 }
