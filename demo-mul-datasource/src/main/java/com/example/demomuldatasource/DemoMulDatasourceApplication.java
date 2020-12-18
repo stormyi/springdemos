075f5d79e2739bbe56ffaedce2c381a692a4efef
yangshenrui <yangshenrui@gmail.com>
2020-12-18 09:21:40 +0800

    0.0.2-SNAPSHOT MySQL多数据源配置

--- demo-mul-datasource/src/main/java/com/example/demomuldatasource/DemoMulDatasourceApplication.java
+++ demo-mul-datasource/src/main/java/com/example/demomuldatasource/DemoMulDatasourceApplication.java
@@ -2,17 +2,30 @@ package com.example.demomuldatasource;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class,
+		JdbcTemplateAutoConfiguration.class
+})
 @Slf4j
-public class DemoMulDatasourceApplication implements CommandLineRunner {
+public class DemoMulDatasourceApplication {
 	@Autowired
 	private DataSource dataSource;
 
@@ -20,16 +33,42 @@ public class DemoMulDatasourceApplication implements CommandLineRunner {
 		SpringApplication.run(DemoMulDatasourceApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws SQLException {
-		showSingleConnection();
+	@Bean
+    @ConfigurationProperties("foo.datasource")
+	public DataSourceProperties fooDataSourceProperties() {
+		return new DataSourceProperties();
 	}
 
-	private void showSingleConnection() throws SQLException {
-		log.info(dataSource.toString());
-		Connection conn= dataSource.getConnection();
-		log.info(conn.toString());
-		conn.close();
+	@Bean
+	@Resource
+	@Primary
+	public DataSource fooDataSource(DataSourceProperties fooDataSourceProperties) {
+	    log.info("------foo datasource:{}", fooDataSourceProperties.getUrl());
+		return fooDataSourceProperties.initializeDataSourceBuilder().build();
 	}
 
+	@Bean
+	@Resource
+	public PlatformTransactionManager fooTxManager(DataSource fooDataSource) {
+		return new DataSourceTransactionManager(fooDataSource);
+	}
+
+	@Bean
+	@ConfigurationProperties("bar.datasource")
+	public DataSourceProperties barDataSourceProperties() {
+		return new DataSourceProperties();
+	}
+
+	@Bean
+	@Resource
+	public DataSource barDataSource(DataSourceProperties barDataSourceProperties) {
+		log.info("------foo datasource:{}", barDataSourceProperties.getUrl());
+		return barDataSourceProperties.initializeDataSourceBuilder().build();
+	}
+
+	@Bean
+	@Resource
+	public PlatformTransactionManager barTxManager(DataSource barDataSource) {
+		return new DataSourceTransactionManager(barDataSource);
+	}
 }
